071592200735878ac796f4f3e11a54fe658a15bd
0xkumi <dung.q@autonomous.nyc>
2019-05-28 09:30:43 +0700

    update

--- consensus/bft2/bft.go
+++ consensus/bft2/bft.go
@@ -10,7 +10,6 @@ import (
 	"time"
 )
 
-
 /*
 	Sequence Number: blockheight + round
 */
@@ -21,7 +20,7 @@ const (
 	PREPREPARE = "PREPREPARE"
 	PREPARE    = "PREPARE"
 	COMMIT     = "COMMIT"
-	NEWROUND      = "NEWROUND"
+	NEWROUND   = "NEWROUND"
 )
 
 const (
@@ -33,8 +32,8 @@ const (
 )
 
 const (
-	BEACON_TIME_MIN = 10*time.Second
-	SHARD_TIME_MIN = 5*time.Second
+	BEACON_TIME_MIN = 10 * time.Second
+	SHARD_TIME_MIN  = 5 * time.Second
 )
 
 type serverInterface interface {
@@ -54,8 +53,8 @@ type Block interface {
 
 type Role struct {
 	nodeType string //beacon|shard
-	role string //pending|validator|proposer
-	shardID byte //shardID
+	role     string //pending|validator|proposer
+	shardID  byte   //shardID
 }
 
 type View struct {
@@ -64,7 +63,7 @@ type View struct {
 }
 
 type NextStateCh struct {
-	state *BFTState
+	state     *BFTState
 	nextState string
 }
 
@@ -83,8 +82,8 @@ type CommitMsg struct {
 type BFTState struct {
 	State string //state
 
-	View        View   //view when creating/listening block
-	Block       Block  //message
+	View  View  //view when creating/listening block
+	Block Block //message
 
 	isOk bool //vote for this message
 
@@ -93,54 +92,52 @@ type BFTState struct {
 	Commit        []PrePrepareMsg
 }
 
-
-type BFTEngine struct{
-	UserKeySet *cashec.KeySet
-	Server                   serverInterface
-	newStateCh chan NextStateCh
+type BFTEngine struct {
+	UserKeySet      *cashec.KeySet
+	Server          serverInterface
+	newStateCh      chan NextStateCh
 	ProposedBlockCh chan Block
 	PrePrepareMsgCh chan PrePrepareMsg
-	PrepareMsgCh chan PrepareMsg
-	CommitMsgCh chan CommitMsg
-	ViewMsgCh chan View
+	PrepareMsgCh    chan PrepareMsg
+	CommitMsgCh     chan CommitMsg
+	ViewMsgCh       chan View
 }
 
-func(e *BFTEngine) Start(
-		UserKeySet *cashec.KeySet,
-		Server     serverInterface,
-	) {
+func (e *BFTEngine) Start(
+	UserKeySet *cashec.KeySet,
+	Server serverInterface,
+) {
 	//var stateCache = make(map[uint64]BFTState)
 	//var curentState = BFTState{}
-	
-	
-	ticker := time.Tick(5*time.Second)
-	go func(){
-		for _ = range ticker{
+
+	ticker := time.Tick(5 * time.Second)
+	go func() {
+		for _ = range ticker {
 			_ = e.createCurrentView()
 			//TODO: broadcast view
 		}
 	}()
-	
+
 	for { //data flow
 		select {
-		case s := <- e.newStateCh:
+		case s := <-e.newStateCh:
 			e.nextState(s.state, s.nextState)
-		case <- e.ProposedBlockCh:
-		case <- e.PrePrepareMsgCh:
-		case <- e.PrepareMsgCh:
-		case <- e.CommitMsgCh:
-		case <- e.ViewMsgCh: //update view of other nodes
+		case <-e.ProposedBlockCh:
+		case <-e.PrePrepareMsgCh:
+		case <-e.PrepareMsgCh:
+		case <-e.CommitMsgCh:
+		case <-e.ViewMsgCh: //update view of other nodes
 			//TODO: check for node role and start propose or handleListenPhase
 		}
 	}
 
 }
 
-func(e *BFTEngine) nextState(s *BFTState, nextState string) {
+func (e *BFTEngine) nextState(s *BFTState, nextState string) {
 	if s.State == nextState {
 		return //already transition
 	}
-	
+
 	switch nextState {
 	case PROPOSE:
 		s.State = PROPOSE
@@ -164,7 +161,7 @@ func(e *BFTEngine) nextState(s *BFTState, nextState string) {
 }
 
 // create new block
-func(e *BFTEngine) handleProposePhase(s *BFTState) {
+func (e *BFTEngine) handleProposePhase(s *BFTState) {
 	time.AfterFunc(PROPOSE_TIMEOUT, func() {
 		e.newStateCh <- NextStateCh{s, PREPREPARE}
 	})
@@ -179,7 +176,7 @@ func(e *BFTEngine) handleProposePhase(s *BFTState) {
 }
 
 //listen for block
-func(e *BFTEngine) handleListenPhase(s *BFTState) {
+func (e *BFTEngine) handleListenPhase(s *BFTState) {
 	time.AfterFunc(LISTEN_TIMEOUT, func() {
 		if s.State == LISTEN {
 			e.newStateCh <- NextStateCh{s, PREPREPARE}
@@ -189,7 +186,7 @@ func(e *BFTEngine) handleListenPhase(s *BFTState) {
 
 // send pre-prepare message (sequence number)
 // wait for more than > 2/3 nodes
-func(e *BFTEngine) handlePrePreparePhase(s *BFTState) {
+func (e *BFTEngine) handlePrePreparePhase(s *BFTState) {
 	time.AfterFunc(PRE_PREPARE_TIMEOUT, func() {
 		e.newStateCh <- NextStateCh{s, PREPARE}
 	})
@@ -201,7 +198,7 @@ func(e *BFTEngine) handlePrePreparePhase(s *BFTState) {
 	if s.Block != nil {
 		isValid = e.validateBlockWithCurrentView(s.Block, s.View)
 	}
-	
+
 	e.PrePrepareMsgCh <- PrePrepareMsg{isValid}
 	s.isOk = isValid
 	//TODO: broadcast
@@ -209,7 +206,7 @@ func(e *BFTEngine) handlePrePreparePhase(s *BFTState) {
 
 //send prepare message (signature of that message & sequence number) and wait for > 2/3 signature of nodes
 //block for the message and sequence number
-func(e *BFTEngine) handlePreparePhase(s *BFTState) {
+func (e *BFTEngine) handlePreparePhase(s *BFTState) {
 	time.AfterFunc(PREPARE_TIMEOUT, func() {
 		e.newStateCh <- NextStateCh{s, COMMIT}
 	})
@@ -218,42 +215,41 @@ func(e *BFTEngine) handlePreparePhase(s *BFTState) {
 
 //broadcast handleCommitPhase for a block
 //for those who dont know which state it is/or he de-sync from network, 2/3 handleCommitPhase message will show him
-func(e *BFTEngine) handleCommitPhase(s *BFTState) {
+func (e *BFTEngine) handleCommitPhase(s *BFTState) {
 	time.AfterFunc(COMMIT_TIMEOUT, func() {
-		e.newStateCh <- NextStateCh{s, REPLY}
+		e.newStateCh <- NextStateCh{s, NEWROUND}
 	})
 	//There are replicas (non-faulty or otherwise) that didn't receive enough (i.e. 2f+1) PREPARE messages, either due to lossy network or being offline for a while. For them, they can't reach PREPARED stage. But! But when they heard from 2f+1 replicas broadcasting COMMIT message, they could be certain to handleCommitPhase on (m,v,n,i)
 	//TODO: broadcast
 
 	//TODO: if certain that block is handleCommitPhase , proceed to next state
-	
+
 	//TODO: if block is commit, then insert block to chain and broadcast block
 }
 
-func(e *BFTEngine) handleNewRoundPhase(s *BFTState) {
+func (e *BFTEngine) handleNewRoundPhase(s *BFTState) {
 	//wait for min block time
 }
 
-
-func(e *BFTEngine) validateBlockWithCurrentView(b Block, v View) bool {
+func (e *BFTEngine) validateBlockWithCurrentView(b Block, v View) bool {
 	return true
 }
 
-func(e *BFTEngine) createNewBlockFromCurrentView(v View) Block {
+func (e *BFTEngine) createNewBlockFromCurrentView(v View) Block {
 	return "sd"
 }
 
-func(e *BFTEngine) createCurrentView() View{
+func (e *BFTEngine) createCurrentView() View {
 	view := View{}
-	nodeType,shardID := blockchain.GetBestStateBeacon().GetPubkeyNodeRole(e.UserKeySet.GetPublicKeyB58())
+	nodeType, shardID := blockchain.GetBestStateBeacon().GetPubkeyNodeRole(e.UserKeySet.GetPublicKeyB58())
 	round := e.predictCurrentRound(nodeType, shardID)
 	role := blockchain.GetBestStateBeacon().GetPubkeyChainRole(e.UserKeySet.GetPublicKeyB58(), round)
-	view.Role = Role{nodeType,role,shardID}
+	view.Role = Role{nodeType, role, shardID}
 	view.Round = round
 	return view
 }
 
-func (e *BFTEngine) predictCurrentRound(nodeType string, shardID byte) int{
+func (e *BFTEngine) predictCurrentRound(nodeType string, shardID byte) int {
 	timeSinceLastBlk := time.Since(time.Unix(0, 0))
 	if nodeType == common.BEACON_ROLE {
 		timeSinceLastBlk = time.Since(time.Unix(blockchain.GetBestStateBeacon().BestBlock.Header.Timestamp, 0))
@@ -262,11 +258,11 @@ func (e *BFTEngine) predictCurrentRound(nodeType string, shardID byte) int{
 	} else {
 		return 0
 	}
-	
+
 	roundTime := LISTEN_TIMEOUT + PRE_PREPARE_TIMEOUT + PREPARE_TIMEOUT + COMMIT_TIMEOUT
 	if timeSinceLastBlk > roundTime {
-		return int(math.Floor(timeSinceLastBlk.Seconds()/ roundTime.Seconds()))
+		return int(math.Floor(timeSinceLastBlk.Seconds() / roundTime.Seconds()))
 	}
-	
+
 	return 0
 }
