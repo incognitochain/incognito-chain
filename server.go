c7c4eaa78f8b1731d5914ce9707a776749fae413
0xbunyip <>
2019-11-21 10:39:18 +0700

    Fix checking IsTestnet()

--- server.go
+++ server.go
@@ -688,7 +688,7 @@ func (serverObj Server) Start() {
 	if serverObj.chainParams.CheckForce {
 		serverObj.CheckForceUpdateSourceCode()
 	}*/
-	if cfg.TestNet == "testnet" {
+	if cfg.IsTestnet() {
 		Logger.log.Critical("************************" +
 			"* Testnet is active *" +
 			"************************")
