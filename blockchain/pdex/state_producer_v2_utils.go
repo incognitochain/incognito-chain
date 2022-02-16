fb26084e024f87a151cfe1a19bc84e992b0f079a
tin-incognito <tin@incognito.org>
2022-02-16 11:24:39 +0700

    [pdex] Try to solve comments - pt 3

--- blockchain/pdex/state_producer_v2_utils.go
+++ blockchain/pdex/state_producer_v2_utils.go
@@ -19,20 +19,20 @@ func (sp *stateProducerV2) validateContributions(
 	if contribution0.PoolPairID() != contribution1.PoolPairID() {
 		return errors.New("contribution 0 and contribution 1 need to be same poolPairID")
 	}
-	if contribution0.OtaReceiver() != utils.EmptyString && contribution1.OtaReceiver() != utils.EmptyString {
+	contribution0UseOtaReceiver := contribution0.OtaReceiver() != utils.EmptyString
+	contribution1UseOtaReceiver := contribution1.OtaReceiver() != utils.EmptyString
+	if contribution0UseOtaReceiver && contribution1UseOtaReceiver {
 		if contribution0.NftID().String() != contribution1.NftID().String() {
 			return errors.New("contribution 0 and contribution 1 need to be same nftID")
 		}
-	}
-	if contribution0.OtaReceiver() == utils.EmptyString && contribution1.OtaReceiver() == utils.EmptyString {
+	} else if !contribution0UseOtaReceiver && !contribution1UseOtaReceiver {
 		if contribution0.AccessOTA() == nil && contribution1.AccessOTA() == nil {
 			if contribution0.NftID().String() != contribution1.NftID().String() {
 				return errors.New("contribution 0 and contribution 1 need to be same accessID")
 			}
 		}
-	}
-	if (contribution0.OtaReceiver() == utils.EmptyString) != (contribution1.OtaReceiver() == utils.EmptyString) {
-		return errors.New("both contributions need to have or not have otaReceiver uniformly")
+	} else if (!contribution0UseOtaReceiver) != (!contribution1UseOtaReceiver) {
+		return errors.New("both contributions must share OtaReceiver format")
 	}
 	return nil
 }
